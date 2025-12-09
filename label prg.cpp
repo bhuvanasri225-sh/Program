0b21914391c9b672ffd0a300d6beb8de65fae9f2
Harshitha <bhuvanasri225@gmail.com>
2025-12-09 20:28:12 +0530

    Create label prg.cpp

---
+++ label prg.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a=1;
+	aditya:
+		printf("\nwelcome to aditya university");
+		printf("\nsurampalem\tadb road");
+		a++;
+		if (a<=10)
+		{
+			goto aditya;
+		}
+		return 0;
+}

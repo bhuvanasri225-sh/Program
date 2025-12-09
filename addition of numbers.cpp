7c4506dceae9fa94797c98b4dc2d2155b0e73253
Harshitha <bhuvanasri225@gmail.com>
2025-12-09 20:19:05 +0530

    Create addition of numbers.cpp

---
+++ addition of numbers.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c;
+	printf("Enter a value=");
+	scanf("%d",&a);
+	printf("\nEnter b value=");
+	scanf("%d",&b);
+	c=a-b;
+	printf("\nThe value of c is %d",c);
+}

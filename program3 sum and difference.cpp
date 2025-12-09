8b5e9c881e1fc9d7b04e450bb80de1663b03aa83
Harshitha <bhuvanasri225@gmail.com>
2025-12-09 20:29:59 +0530

    Create program3 sum and difference.cpp

---
+++ program3 sum and difference.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	int a,b,sum,difference;
+	printf("enter a and b values:");
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	difference=a-b;
+	printf("sum of two numbers:%d",sum);
+	printf("\ndifference of two numbers:%d",difference);
+}

663acd01de0381fa00caa96002a96bac29972eda
Harshitha <bhuvanasri225@gmail.com>
2025-12-09 20:32:56 +0530

    Create while loop1.cpp

---
+++ while loop1.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n,r=0,s=0;
+	printf("Enter n value:");
+	scanf("%d",&n);
+	while(n>0)
+	{
+		r=n%10;
+		s=s+r;
+		n=n/10;
+	}
+	printf("\nSum of digits=%d",s);
+	return 0;
+}
+

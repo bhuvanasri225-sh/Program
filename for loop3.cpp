ca4aebd17fc82bd84dfd4985a094b3aedddaf984
Harshitha <bhuvanasri225@gmail.com>
2025-12-09 20:27:42 +0530

    Create for loop3.cpp

---
+++ for loop3.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n,count=0;
+	printf("Enter n value:");
+	scanf("%d",&n);
+	for(i=2;i<=n;i++)
+	{
+		count=0;
+		for(j=1;j<=i;j++)
+		{
+			if(i%j==0)
+			{
+				count++;
+			}
+		}
+		if(count==2)
+	        {
+	        	printf("%d is a prime number\n",i);
+	        }
+     	else
+         	{
+	        	printf("%d is not a prime number\n",i);
+	        }
+	}
+
+	return 0;
+}

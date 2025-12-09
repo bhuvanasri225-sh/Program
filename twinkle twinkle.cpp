438312501e12c00d5c31f60446e5f824ca47e7c8
Harshitha <bhuvanasri225@gmail.com>
2025-12-09 20:32:22 +0530

    Create twinkle twinkle.cpp

---
+++ twinkle twinkle.cpp
@@ -0,0 +1,5 @@
+#include<stdio.h>
+main()
+{
+	printf("Twinkle,twinkle,little star,\n\tHow I Wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle,twinkle,little star,\n\tHow I wonder what you are!");
+}

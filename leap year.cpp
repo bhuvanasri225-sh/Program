7f9bed2097c28b14abed7a616131b1788162e8bd
Harshitha <bhuvanasri225@gmail.com>
2025-12-09 20:28:17 +0530

    Create leap year.cpp

---
+++ leap year.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int year;
+	printf("\n enter year");
+	scanf("%d",&year);
+	if (year%4==0&&year%100!=0||year%400==0)
+	{
+		printf("\nleap year");
+	}
+	else
+	{
+		printf("not a leap year");
+	}
+	return 0;
+}

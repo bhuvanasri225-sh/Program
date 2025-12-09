36e677e8d1e1337d6317bf6c292f487d80cec7b8
Harshitha <bhuvanasri225@gmail.com>
2025-12-09 20:28:34 +0530

    Create local variables2.cpp

---
+++ local variables2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a=10;
+	printf("\nA value is=%d",a);
+	if(a==10)
+	{
+		int a=20;
+		printf("\nA value is=%d",a);
+	}
+	printf("\nA value is=%d",a);
+	return 0;
+}

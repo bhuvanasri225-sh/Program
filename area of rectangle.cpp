16aad613af1900f3ecb798fcaaadc56a30d8719d
Harshitha <bhuvanasri225@gmail.com>
2025-12-09 20:19:19 +0530

    Create area of rectangle.cpp

---
+++ area of rectangle.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	int l,b,area;
+	printf("The value of l=");
+	scanf("%d",&l);
+	printf("The value of b=");
+	scanf("%d",&b);
+	area=l*b;
+	printf("The area of rectangle is %d",area);
+}
